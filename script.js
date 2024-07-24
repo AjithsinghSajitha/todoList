860bd4933fe87083403fc3d2732f5d8873535866
Ajithsingh Athmasaran Sajitha <s.ajithsingh@noxum.com>
2024-07-24 10:55:52 +0530

    added necessary comments and re-arranged the functions

--- script.js
+++ script.js
@@ -6,57 +6,20 @@ const completed = document.getElementById("completed");
 
 const toDoList = [];
 
-//Filter the toDoList and collect only the completed tasks length
-const setCompleted = () => {
-  let finishedTask = toDoList.filter((item) => item.done === true);
-  completed.innerText = `Completed: ${finishedTask.length}`;
-};
-
-//Removes the item permanently from the list by removing the item from toDoList call the renderList function
-const setRemove = (index) => {
-  let remove = document.createElement("span");
-  remove.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-  remove.setAttribute("remove-id", index);
-  remove.addEventListener("click", () => {
-    toDoList.splice(index, 1);
+//This will add user input to the list and set the focus to input
+const addItem = () => {
+  addBtn.addEventListener("click", () => {
+    if (userInput.value) toDoList.push({ do: userInput.value, done: false });
+    userInput.value = "";
+    userInput.focus();
     renderList(toDoList);
-    setCompleted();
-    total.innerText = `Total: ${toDoList.length}`;
   });
-
-  return remove;
 };
 
-//Set the status of the list based on the check-box state
-const setCheckBox = (index, mainDiv, doText) => {
-  let checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("check-id", index);
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked) {
-      mainDiv.classList.add("completed");
-      toDoList[index].done = true;
-      doText.classList.add("strike");
-    } else {
-      toDoList[index].done = false;
-      mainDiv.classList.remove("completed");
-      doText.classList.remove("strike");
-    }
-    setCompleted();
-  });
-  return checkbox;
-};
-
-//Collect the text from toDoList array and add that inside a span
-const setToDoText = (text) => {
-  let toDoText = document.createElement("span");
-  toDoText.classList.add("todo-text");
-  toDoText.setAttribute("title", text);
-  toDoText.innerText = text;
-  return toDoText;
-};
-
-//Clicking on the add button will call this and render the created list
+/**
+* Clicking on the add button will call this and render the created list
+* @param {Array} toDoList - List array for mapping
+*/
 const renderList = (toDoList) => {
   main[0].innerHTML = "";
   toDoList.map((item, index) => {
@@ -79,14 +42,69 @@ const renderList = (toDoList) => {
   });
 };
 
-//This will add user input to the list and set the focus to input
-const addItem = () => {
-  addBtn.addEventListener("click", () => {
-    if (userInput.value) toDoList.push({ do: userInput.value, done: false });
-    userInput.value = "";
-    userInput.focus();
-    renderList(toDoList);
+/**
+* Collect the text from toDoList array and add that inside a span
+* @param {number} text - do text
+* @returns - a span element
+*/
+const setToDoText = (text) => {
+  let toDoText = document.createElement("span");
+  toDoText.classList.add("todo-text");
+  toDoText.setAttribute("title", text);
+  toDoText.innerText = text;
+  return toDoText;
+};
+
+/** 
+ * Set the status of the list based on the check-box state
+ * @param {number} index - index for check-id attribute
+ * @param {HTMLDivElement} mainDiv - HTMLDivElement for each to do list
+ * @param {text} doText - do text
+ * @returns - an input element
+ */
+const setCheckBox = (index, mainDiv, doText) => {
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("check-id", index);
+  checkbox.addEventListener("click", () => {
+    if (checkbox.checked) {
+      mainDiv.classList.add("completed");
+      toDoList[index].done = true;
+      doText.classList.add("strike");
+    } else {
+      toDoList[index].done = false;
+      mainDiv.classList.remove("completed");
+      doText.classList.remove("strike");
+    }
+    setCompleted();
   });
+  return checkbox;
+};
+
+/**
+* Removes the item permanently from the list by removing the item from 
+* toDoList by splicing and calls the renderList function
+* @param {number} index - index for toDoList
+* @returns - a span element
+*/
+const setRemove = (index) => {
+  let remove = document.createElement("span");
+  remove.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  remove.setAttribute("remove-id", index);
+  remove.addEventListener("click", () => {
+    toDoList.splice(index, 1);
+    renderList(toDoList);
+    setCompleted();
+    total.innerText = `Total: ${toDoList.length}`;
+  });
+
+  return remove;
+};
+
+//Filter the toDoList and collect only the completed tasks length
+const setCompleted = () => {
+  let finishedTask = toDoList.filter((item) => item.done === true);
+  completed.innerText = `Completed: ${finishedTask.length}`;
 };
 
 (function init() {
